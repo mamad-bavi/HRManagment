cbed1884f5687097e3c284e2a334b66f00259bac
mohammad Bavi <mohammadbavi142434@hotmail.com>
2026-08-09 16:49:57 +0330

    work project by program.cs and startup.cs i like work by startup.cs and that is not  requierment

--- HRManagment/Program.cs
+++ HRManagment/Program.cs
@@ -1,23 +1,65 @@
-var builder = WebApplication.CreateBuilder(args);
-
-// Add services to the container.
-
-builder.Services.AddControllers();
-// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
-builder.Services.AddOpenApi();
-
-var app = builder.Build();
-
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
+namespace HRManagment
 {
-    app.MapOpenApi();
+    public class Program
+    {
+        public static async Task Main(string[] args)
+        {
+
+
+            //Set deafult proxy
+            //WebRequest.DefaultWebProxy = new WebProxy("http://127.0.0.1:8118", true) { UseDefaultCredentials = true };
+
+            //var logger = NLogBuilder.ConfigureNLog("nlog.config").GetCurrentClassLogger();
+
+            try
+            {
+                //logger.Debug("init main");
+                //logger.Info("Starting application...");
+                ////logger.Trace("");
+                var host = CreateHostBuilder(args).Build();
+
+                //using (var scope = host.Services.CreateScope())
+                //{
+                //    var dbContext = scope.ServiceProvider.GetRequiredService<AppDbContext>();
+                //    var permissionRepository = scope.ServiceProvider.GetRequiredService<IPermissionRepository>();
+                //    //var claimsItems = new AreaControllerActionService().
+                //    //ApiAreaAndActionAndControllerNamesList()
+                //    //.Select(c => new PermissionDto()
+                //    //{
+                //    //    AreaName = c.AreaName,
+                //    //    ControllerName = c.ControllerName,
+                //    //    ActionName = c.ActionName,
+                //    //    NeedAccess = c.NeedAccess,
+                //    //}).ToList();
+
+                //    //await permissionRepository.AddRangeAsync(claimsItems);
+                //}
+
+                await host.RunAsync();
+            }
+            catch (Exception ex)
+            {
+                //NLog: catch setup errors
+                //logger.Error(ex, "Stopped program because of exception");
+                throw;
+            }
+            finally
+            {
+                // Ensure to flush and stop internal timers/threads before application-exit (Avoid segmentation fault on Linux)
+                //NLog.LogManager.Shutdown();
+            }
+
+
+
+        }
+
+        public static IHostBuilder CreateHostBuilder(string[] args) =>
+            Host.CreateDefaultBuilder(args)
+                .ConfigureLogging(option => option.ClearProviders())
+                .ConfigureWebHostDefaults(webBuilder =>
+                {
+                    webBuilder.UseStartup<Startup>();
+                });
+    }
+
 }
-
-app.UseHttpsRedirection();
-
-app.UseAuthorization();
-
-app.MapControllers();
-
-app.Run();
---
+++ HRManagment/Startup.cs
@@ -0,0 +1,58 @@
+﻿
+using System.Text.Json.Serialization;
+
+
+
+namespace HRManagment
+{
+    public class Startup
+    {
+        //private readonly SiteSettings _siteSettings;
+
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+            //AutoMapperConfiguration.InitializeAutoMapper();
+           // _siteSettings = Configuration.GetSection(nameof(SiteSettings)).Get<SiteSettings>();
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddControllers();
+            services.AddMvc();
+            // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
+            services.AddOpenApi();
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            // Configure the HTTP request pipeline.
+            if (!env.IsDevelopment())
+            {
+                //app.MapOpenApi();
+                app.UseHsts();
+            }
+
+            app.UseRouting();
+
+            //app.UseMvc();
+            app.UseHttpsRedirection();
+
+            app.UseAuthorization();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllers();
+            });
+
+        }
+
+
+
+    }
+}
+
