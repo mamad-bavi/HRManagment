8640e609be05352ec8dd020bbb0a8e148c939f78
mohammad Bavi <mohammadbavi142434@hotmail.com>
2026-08-09 17:05:14 +0330

    Clean arch

---
+++ SRC/Core/Domin/Entities/Base/BaseEntity.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Domin.Entities.Base
+{
+
+public abstract class BaseEntity
+{
+    public long Id { get; set; }
+
+    public long? CreateDate { get; set; }
+    public long? CreateUserId { get; set; }
+    public long? ModifyDate { get; set; }
+    public long? ModifyUserId { get; set; }
+    public long? DeletedUserId { get; set; }
+    public bool? IsDeleted { get; set; }
+}
+
+}
+
