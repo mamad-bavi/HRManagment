ef68469759ce47e2f6c38431b9169e4c185caef0
mohammad Bavi <mohammadbavi142434@hotmail.com>
2026-08-09 17:20:20 +0330

    create base dto

--- SRC/Core/Domin/Entities/Base/BaseEntity.cs
+++ SRC/Core/Domin/Entities/Base/BaseEntity.cs
@@ -5,17 +5,30 @@ using System.Text;
 namespace Domin.Entities.Base
 {
 
-public abstract class BaseEntity
-{
-    public long Id { get; set; }
+    public interface IBaseEntity
+    {
+    }
 
-    public long? CreateDate { get; set; }
-    public long? CreateUserId { get; set; }
-    public long? ModifyDate { get; set; }
-    public long? ModifyUserId { get; set; }
-    public long? DeletedUserId { get; set; }
-    public bool? IsDeleted { get; set; }
-}
+    public abstract class BaseEntity<TKey,TDateProperty> : IBaseEntity
+    {
+        public TKey Id { get; set; }
+
+        public TDateProperty? CreateDate { get; set; }
+        public TKey? CreateUserId { get; set; }
+        public TDateProperty? ModifyDate { get; set; }
+        public TKey? ModifyUserId { get; set; }
+        public TDateProperty DeletedDate { get; set; }
+        public TKey? DeletedUserId { get; set; }
+        public bool? IsDeleted { get; set; }
+    }
+
+    public abstract class BaseEntity<TDateProperty> : BaseEntity<long, TDateProperty>
+    {
+    }
+
+    public abstract class BaseEntity:BaseEntity<long>
+    {
+    }
 
 }
 
