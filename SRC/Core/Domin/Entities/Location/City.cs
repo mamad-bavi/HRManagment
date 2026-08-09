79d02d902abb3c8224551df56d71366cb1156b0d
mohammad Bavi <mohammadbavi142434@hotmail.com>
2026-08-09 18:01:03 +0330

    Create the entity we need to create a new person and organ and job possition

---
+++ SRC/Core/Domin/Entities/Location/City.cs
@@ -0,0 +1,15 @@
+﻿using Domin.Entities.Base;
+
+namespace Domin.Entities.Location
+{
+    public class City:BaseEntity
+    {
+        public string Name { get; set; }
+
+        public long ProvinceId { get; set; }
+        public virtual Province Province { get; set; }
+        public virtual ICollection<Organization> Organizations { get; set; }
+
+
+    }
+}
---
+++ SRC/Core/Domin/Entities/People/JobPositionPerson.cs
@@ -0,0 +1,17 @@
+﻿using Domin.Entities.Base;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Domin.Entities.People
+{
+    public class JobPositionPerson:BaseEntity
+    {
+        public long JobPositionId { get; set; }
+        public virtual JobPosition JobPosition { get; set; }
+
+        public long PersonId { get; set; }
+        public virtual Person Person { get; set; }
+
+    }
+}
