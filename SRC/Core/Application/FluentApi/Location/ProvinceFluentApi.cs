385b4c2c1a87e10ef6712e2753da72f7f6ef27f8
mohammad Bavi <mohammadbavi142434@hotmail.com>
2026-08-12 22:39:57 +0330

    Fixed bugs and create FluentApi for Location items

---
+++ SRC/Core/Application/FluentApi/Location/ProvinceFluentApi.cs
@@ -0,0 +1,32 @@
+﻿using Domain.Entities.Location;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Application.FluentApi.Location
+{
+    public class ProvinceFluentApi : IEntityTypeConfiguration<Province>
+    {
+        public void Configure(EntityTypeBuilder<Province> builder)
+        {
+            builder.ToTable(nameof(Province));
+
+            builder.HasKey(c => c.Id);
+
+            builder.Property(c => c.Name)
+                .IsRequired()
+                .HasMaxLength(100);
+
+            builder.HasMany(c => c.Cities)
+               .WithOne(c => c.Province)
+               .HasForeignKey(c => c.ProvinceId);
+
+            builder.HasMany(c => c.Organizations)
+               .WithOne(c => c.Province)
+               .HasForeignKey(c => c.ProvinceId);
+
+        }
+    }
+}
--- SRC/Core/Domain/Entities/Location/Organization.cs
+++ SRC/Core/Domain/Entities/Location/Organization.cs
@@ -17,9 +17,9 @@ namespace Domain.Entities.Location
 
         public string Address { get; set; }
 
-        public double Longitude { get; set; }
+        public double? Longitude { get; set; }
 
-        public double Latitude { get; set; }
+        public double? Latitude { get; set; }
 
         public virtual Province Province { get; set; }
 
