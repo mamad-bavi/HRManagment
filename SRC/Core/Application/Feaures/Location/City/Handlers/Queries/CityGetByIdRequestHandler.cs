96a05296b3d6ef1ceba29450d491e3239594c435
mohammad Bavi <mohammadbavi142434@hotmail.com>
2026-08-12 15:20:11 +0330

    compelete Location Queries

---
+++ SRC/Core/Application/Feaures/Location/City/Handlers/Queries/CityGetByIdRequestHandler.cs
@@ -0,0 +1,26 @@
+﻿using Application.Contracts.Location;
+using Application.DTOs.Location.CityDtos;
+using Application.Feaures.Location.City.Requests.Queries;
+using Application.Utilities.AutoMapperGeneric;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Application.Feaures.Location.City.Handlers.Queries
+{
+    public class CityGetByIdRequestHandler : IRequestHandler<CityGetByIdRequest, CityGetByIdDto>
+    {
+        private readonly ICityRepository cityRepository;
+
+        public CityGetByIdRequestHandler(ICityRepository cityRepository)
+        {
+            this.cityRepository = cityRepository;
+        }
+        public async Task<CityGetByIdDto> Handle(CityGetByIdRequest request, CancellationToken cancellationToken)
+        {
+            var resualt = await cityRepository.GetByIdAsync(cancellationToken, request.Id);
+            return resualt.ConvertObject<CityGetByIdDto, Domain.Entities.Location.City>();
+        }
+    }
+}
