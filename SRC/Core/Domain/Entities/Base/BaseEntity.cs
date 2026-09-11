5160cd216171e85c59f98084a8f74e0416b01b4c
mohammad Bavi <mohammadbavi142434@hotmail.com>
2026-09-11 13:11:10 +0330

    Create the generic repository as nupkg and add tothis project

--- SRC/Core/Domain/Entities/Base/BaseEntity.cs
+++ SRC/Core/Domain/Entities/Base/BaseEntity.cs
@@ -15,10 +15,10 @@ namespace Domain.Entities.Base
         public TKey? CreateUserId { get; set; }
         public TDateProperty? ModifyDate { get; set; }
         public TKey? ModifyUserId { get; set; }
+        public byte[] RowVersion { get; set; }
         public TDateProperty DeletedDate { get; set; }
         public TKey? DeletedUserId { get; set; }
         public bool? IsDeleted { get; set; }
-        public byte[] RowVersion { get; set; }
     }
 
     public abstract class BaseEntity<TDateProperty> : BaseEntity<long, TDateProperty>
