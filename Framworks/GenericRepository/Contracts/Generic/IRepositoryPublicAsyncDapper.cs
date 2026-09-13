2c6189ae5f26a4211c28945359972558b400d8a9
mohammad Bavi <mohammadbavi142434@hotmail.com>
2026-09-14 00:58:50 +0330

    generic repository

---
+++ Framworks/GenericRepository/Contracts/Generic/IRepositoryPublicAsyncDapper.cs
@@ -0,0 +1,17 @@
+﻿using GenericRepository.Filters;
+
+namespace GenericRepository.Contracts.Generic
+{
+    public interface IRepositoryPublicAsyncDapper<TEntity> where TEntity : class
+    {
+        
+        Task<TEntity> GetByIdQueryAsync(params object[] Id);
+        Task<GreadData<TEntity>> GetByIdDeletedItemQueryAsync(params object[] Id);
+        Task<GreadData<TEntity>> GetByQueryAsync(CancellationToken cancellationToken, GreadData<TEntity> data);
+        Task<GreadData<TEntity>> GetByQueryDeletedItemsAsync(CancellationToken cancellationToken, GreadData<TEntity> data);
+        Task<GreadData<TEntity>> GetByRangIdQuerAsync(params object[] Ids);
+        Task<bool> AddByDapperAsync(TEntity entity);
+        Task<bool> UpdateByDapperAsync(TEntity entity);
+
+    }
+}
---
+++ Framworks/GenericRepository/Settings/AssembliesSetting.cs
@@ -0,0 +1,10 @@
+﻿using System.Reflection;
+
+namespace GenericRepository.Settings
+{
+    public class AssembliesSetting
+    {
+        public Assembly[] EntitiesAssemblies { get; set; } = [];
+        public Assembly[] EntitiesConfigurationAssemblies { get; set; } = [];
+    }
+}
